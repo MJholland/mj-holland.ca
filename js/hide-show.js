15d43ca60cffbb92f53e7083167f2f634d29a605
MJholland <mjzr@outlook.com>
2017-11-30 13:43:26 -0500

    Header rotations are now working
    Took a lot of thinking and staring at the code.

--- js/hide-show.js
+++ js/hide-show.js
@@ -1,81 +1,77 @@
+//variables for show/hide div
 var x = document.getElementById("aboutMe");
 var y = document.getElementById("work");
 var z = document.getElementById("contact");
+//variables for rotating header
 var a = document.getElementById("aboutheader").classList;
 var b = document.getElementById("workheader").classList;
 var c = document.getElementById("contactheader").classList;
-//about me show hide, also closes other sections so only one displays
+
+//about me show hide & rotation. also closes & rotates other sections so only one displays
 function openCloseAM() {
   "use strict";
   if (!x.style.display || x.style.display === "none") {
       x.style.display = "block";
+      a.remove("rotateheader");
+      a.add("levelheader");
   } else {
       x.style.display = "none";
+      a.remove("levelheader");
+      a.add("rotateheader");
   }
-//for the other sections to show and hide
-if (y.style.display === "block" || z.style.display === "block") {
-  y.style.display = "none";
-  z.style.display = "none";
-  }
-
-  if (a.contains("rotateheader")) {
-    a.remove("rotateheader");
-  } else {
-    a.add("rotateheader");
-  }
-  if (a.contains("levelheader")) {
-    a.remove("levelheader");
-  } else {
-    a.add("levelheader");
+//for the other sections to show/hide & rotate
+  if (y.style.display === "block" || z.style.display === "block") {
+    y.style.display = "none";
+    z.style.display = "none";
+    b.remove("levelheader");
+    b.add("rotateheader");
+    c.remove("levelheader");
+    c.add("rotateheader");
   }
 }
 
+//work show hide & rotation. also closes & rotates other sections so only one displays
 function openCloseW() {
   "use strict";
   if (!y.style.display || y.style.display === "none") {
       y.style.display = "block";
+      b.remove("rotateheader");
+      b.add("levelheader");
   } else {
       y.style.display = "none";
+      b.remove("levelheader");
+      b.add("rotateheader");
   }
-
+//for the other sections to show/hide & rotate
   if (x.style.display === "block" || z.style.display === "block") {
     x.style.display = "none";
     z.style.display = "none";
-  }
-
-  if (b.contains("rotateheader")) {
-    b.remove("rotateheader");
-  } else {
-    b.add("rotateheader");
-  }
-  if (b.contains("levelheader")) {
-    b.remove("levelheader");
-  } else {
-    b.add("levelheader");
+    a.remove("levelheader");
+    a.add("rotateheader");
+    c.remove("levelheader");
+    c.add("rotateheader");
   }
 }
 
+//contact show hide & rotation. also closes & rotates other sections so only one displays
 function openCloseC() {
   "use strict";
   if (!z.style.display || z.style.display === "none") {
     z.style.display = "block";
+    c.remove("rotateheader");
+    c.add("levelheader");
   } else {
     z.style.display = "none";
+    c.remove("levelheader");
+    c.add("rotateheader");
   }
-
+  //for the other sections to show/hide & rotate
   if (x.style.display === "block" || y.style.display === "block") {
     x.style.display = "none";
     y.style.display = "none";
-  }
-
-  if (c.contains("rotateheader")) {
-    c.remove("rotateheader");
-  } else {
-    c.add("rotateheader");
-  }
-  if (c.contains("levelheader")) {
-    c.remove("levelheader");
-  } else {
-    c.add("levelheader");
+    a.remove("levelheader");
+    a.add("rotateheader");
+    b.remove("levelheader");
+    b.add("rotateheader");
   }
 }
