e7d690e27506381b7e61b9a8162efbd4c05a12d9
MJholland <mjzr@outlook.com>
2017-12-01 09:14:12 -0500

    Completely changed js
    Smaller code, animations are now transitions. Changed some spacing in html.

--- js/hide-show.js
+++ js/hide-show.js
@@ -1,7 +1,3 @@
-//variables for show/hide div
-var x = document.getElementById("aboutMe");
-var y = document.getElementById("work");
-var z = document.getElementById("contact");
 //variables for rotating header
 var r1 = document.getElementById("aboutheader").classList;
 var r2 = document.getElementById("workheader").classList;
@@ -13,77 +9,47 @@ var s3 = document.getElementById("contact").classList;
 //about me show hide & rotation. also closes & rotates other sections so only one displays
 function openCloseAM() {
   "use strict";
-  if (!x.style.display || x.style.display === "none") {
-      x.style.display = "block";
-      r1.remove("rotateheader");
-      r1.add("levelheader");
-      s1.add("content-grow")
+  if (s1.contains("content-grow-open")) {
+    s1.remove("content-grow-open");
+    r1.remove("levelheader");
   } else {
-      x.style.display = "none";
-      r1.remove("levelheader");
-      r1.add("rotateheader");
-      s1.remove("content-grow");
-      s1.add("content-shrink");
-  }
-//for the other sections to show/hide & rotate
-  if (y.style.display === "block" || z.style.display === "block") {
-    y.style.display = "none";
-    z.style.display = "none";
+    s1.add("content-grow-open");
+    r1.add("levelheader");
+    s2.remove("content-grow-open");
+    s3.remove("content-grow-open");
     r2.remove("levelheader");
-    r2.add("rotateheader");
     r3.remove("levelheader");
-    r3.add("rotateheader");
   }
 }
 
 //work show hide & rotation. also closes & rotates other sections so only one displays
 function openCloseW() {
   "use strict";
-  if (!y.style.display || y.style.display === "none") {
-      y.style.display = "block";
-      r2.remove("rotateheader");
-      r2.add("levelheader");
-      s2.add("content-grow")
+  if (s2.contains("content-grow-open")) {
+    s2.remove("content-grow-open");
+    r2.remove("levelheader");
   } else {
-      y.style.display = "none";
-      r2.remove("levelheader");
-      r2.add("rotateheader");
-      s2.remove("content-grow");
-      s2.add("content-shrink");
-  }
-//for the other sections to show/hide & rotate
-  if (x.style.display === "block" || z.style.display === "block") {
-    x.style.display = "none";
-    z.style.display = "none";
+    s2.add("content-grow-open");
+    r2.add("levelheader");
+    s1.remove("content-grow-open");
+    s3.remove("content-grow-open");
     r1.remove("levelheader");
-    r1.add("rotateheader");
     r3.remove("levelheader");
-    r3.add("rotateheader");
   }
 }
 
 //contact show hide & rotation. also closes & rotates other sections so only one displays
 function openCloseC() {
   "use strict";
-  if (!z.style.display || z.style.display === "none") {
-    z.style.display = "block";
-    r3.remove("rotateheader");
-    r3.add("levelheader");
-    s3.add("content-grow")
-  } else {
-    z.style.display = "none";
+  if (s3.contains("content-grow-open")) {
+    s3.remove("content-grow-open");
     r3.remove("levelheader");
-    r3.add("rotateheader");
-    s3.remove("content-grow");
-    s3.add("content-shrink");
-  }
-  //for the other sections to show/hide & rotate
-  if (x.style.display === "block" || y.style.display === "block") {
-    x.style.display = "none";
-    y.style.display = "none";
+  } else {
+    s3.add("content-grow-open");
+    r3.add("levelheader");
+    s2.remove("content-grow-open");
+    s1.remove("content-grow-open");
     r1.remove("levelheader");
-    r1.add("rotateheader");
     r2.remove("levelheader");
-    r2.add("rotateheader");
   }
 }
